f82eb4cc764069126904b39cc3334c7448457a4f
clevessonmendonca <wanderbloks311@gmail.com>
2024-01-02 22:17:12 -0300

    feat: sonner and sign screens

---
+++ src/pages/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './_layouts/app'
+import { AuthLayout } from './_layouts/auth'
+import { Dashboard } from './app/dashboard'
+import { SignIn } from './auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/sign-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
+])
